0b52bb88086918db9eaf9aa8fc7b92192f1273d6
Michal Gawik <gawlik.zator@o2.pl>
2024-07-15 18:00:25 +0200

    create navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import LinksDropdown from './LinksDropdown'
+import { UserButton } from '@clerk/nextjs'
+import ThemeToggle from './ThemeToggle'
 
 const Navbar = () => {
-	return <div>Navbar</div>
+	return (
+		<nav className='bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between'>
+			<div>
+				<LinksDropdown />
+			</div>
+			<div className='flex items-center gap-x-4'>
+				<ThemeToggle />
+				<UserButton />
+			</div>
+		</nav>
+	)
 }
 
 export default Navbar
