3e6928cf05d85c62ee860b21ff02dca7cef057fd
Michal Gawik <gawlik.zator@o2.pl>
2024-07-15 17:50:51 +0200

    create sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,15 +2,15 @@ import Navbar from '@/components/Navbar'
 import Sidebar from '@/components/Sidebar'
 import { PropsWithChildren } from 'react'
 
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: PropsWithChildren) => {
 	return (
-		<main className='grid'>
-			<div className='sm:hidden'>
+		<main className='grid lg:grid-cols-5'>
+			<div className='hidden lg:block lg:col-span-1 lg:min-h-screen'>
 				<Sidebar />
 			</div>
-			<div className='lg:grid-cols-4'>
+			<div className='lg:col-span-4'>
 				<Navbar />
-				<div className=''>{children}</div>
+				<div className='py-16 px-4 sm:px-8 lg:px-16'>{children}</div>
 			</div>
 		</main>
 	)
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,33 @@
-import React from 'react'
+'use client'
+
+import logo from '../assets/logo.svg'
+import { links } from '@/utils/links'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = () => {
-	return <div>sidebar</div>
+	const pathname = usePathname()
+
+	return (
+		<aside className='py-4 px-8 bg-muted h-full'>
+			<Image className='mx-auto' src={logo} alt='logo' />
+			<div className='flex flex-col mt-20 gap-y-4'>
+				{links.map(link => {
+					const isActivePage = pathname === link.href
+					const variant = isActivePage ? 'default' : 'link'
+					return (
+						<Button asChild key={link.href} variant={variant}>
+							<Link className='flex items-center gap-x-2 ' href={link.href}>
+								{link.icon} <span className='capitalize'>{link.label}</span>
+							</Link>
+						</Button>
+					)
+				})}
+			</div>
+		</aside>
+	)
 }
 
 export default Sidebar
