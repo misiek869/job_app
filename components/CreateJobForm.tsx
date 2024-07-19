00377b40ebf30f7dccae8b8c99d73f13258934fc
Michal Gawik <gawlik.zator@o2.pl>
2024-07-19 15:05:25 +0200

    create CreateJobForm component

--- components/CreateJobForm.tsx
+++ components/CreateJobForm.tsx
@@ -1,7 +1,58 @@
-import React from 'react'
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+	username: z
+		.string()
+		.min(3, { message: 'Użytkownik musi mieć minimum 3 znaki' }),
+})
 
 const CreateJobForm = () => {
-	return <div>CreateJobForm</div>
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			username: '',
+		},
+	})
+
+	const onSubmit = (values: z.infer<typeof formSchema>) => {
+		console.log(values)
+	}
+
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
+				<FormField
+					control={form.control}
+					name='username'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Username</FormLabel>
+							<FormControl>
+								<Input placeholder='shadcn' {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type='submit'>Submit</Button>
+			</form>
+		</Form>
+	)
 }
 
 export default CreateJobForm
