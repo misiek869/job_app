29357cf204b98dc2280961efb1b0746849c6c2c1
Michal Gawik <gawlik.zator@o2.pl>
2024-07-27 08:27:58 +0200

    create imports

--- components/StatsContainer.tsx
+++ components/StatsContainer.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { getStatsAction } from '@/utils/actions'
+import StatsCard from './StatsCard'
 
 const StatsContainer = () => {
 	return <div>StatsContainer</div>
