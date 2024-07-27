0086019299b57e65037f3cad194dfaaf12d9714a
Michal Gawik <gawlik.zator@o2.pl>
2024-07-27 08:24:45 +0200

    create StatsCard component

--- components/StatsCard.tsx
+++ components/StatsCard.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import {
+	Card,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
 
-const StatsCard = () => {
-	return <div>StatsCard</div>
+import { Skeleton } from './ui/skeleton'
+
+type StatsCardsProps = {
+	title: string
+	value: number
+}
+
+const StatsCard = ({ title, value }: { title: string; value: number }) => {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>{title}</CardTitle>
+				<CardDescription>{value}</CardDescription>
+			</CardHeader>
+		</Card>
+	)
 }
 
 export default StatsCard
