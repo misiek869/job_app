f77bf627a0e2830f673da6f7076951488727a4fb
Michal Gawik <gawlik.zator@o2.pl>
2024-07-28 09:13:35 +0200

    create loading styles

---
+++ app/(dashboard)/stats/loading.tsx
@@ -0,0 +1,12 @@
+import StatsLoadingCard from '@/components/StatsLoadingCard'
+
+function loading() {
+	return (
+		<div className='grid md:grid-cols-2 gap-4 lg:grid-cols-3'>
+			<StatsLoadingCard />
+			<StatsLoadingCard />
+			<StatsLoadingCard />
+		</div>
+	)
+}
+export default loading
--- components/StatsContainer.tsx
+++ components/StatsContainer.tsx
@@ -1,9 +1,23 @@
+'use client'
 import { useQuery } from '@tanstack/react-query'
 import { getStatsAction } from '@/utils/actions'
 import StatsCard from './StatsCard'
+import StatsLoadingCard from './StatsLoadingCard'
+import { Skeleton } from '@/components/ui/skeleton'
 
 const StatsContainer = () => {
-	return <div>StatsContainer</div>
+	const { data } = useQuery({
+		queryKey: ['stats'],
+		queryFn: () => getStatsAction(),
+	})
+
+	return (
+		<div className='grid md:grid-cols-2 gap-4 lg:grid-cols-3'>
+			<StatsCard title='pending jobs' value={data?.pending || 0} />
+			<StatsCard title='interviews set' value={data?.interview || 0} />
+			<StatsCard title='jobs declined' value={data?.declined || 0} />
+		</div>
+	)
 }
 
 export default StatsContainer
