ea27e3da3d0d4ea47d5369370be3297c106f9f9d
Michal Gawik <gawlik.zator@o2.pl>
2024-07-28 13:06:17 +0200

    create logic and styling

--- components/ChartsContainer.tsx
+++ components/ChartsContainer.tsx
@@ -18,18 +18,23 @@ const ChartsContainer = () => {
 		queryFn: () => getChartsDataAction(),
 	})
 
-	if (isPending) {
-		return <h2>Loading...</h2>
-	}
-
-	if (data === null || data?.length < 1) {
-		return null
-	}
+	if (isPending) return <h2 className='text-xl font-medium'>Please wait...</h2>
+	if (!data || data.length < 1) return null
 
 	return (
-		<section>
-			<h1 className=''>Monthly Applications</h1>
-			<ResponsiveContainer></ResponsiveContainer>
+		<section className='mt-16'>
+			<h1 className='text-4xl font-semibold text-center'>
+				Monthly Applications
+			</h1>
+			<ResponsiveContainer width='100%' height={300}>
+				<BarChart data={data} margin={{ top: 50 }}>
+					<CartesianGrid strokeDasharray='3 3' />
+					<XAxis dataKey='date' />
+					<YAxis allowDecimals={false} />
+					<Tooltip />
+					<Bar dataKey='count' fill='#2563eb' barSize={75} />
+				</BarChart>
+			</ResponsiveContainer>
 		</section>
 	)
 }
