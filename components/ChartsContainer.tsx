9f11ac3e04e7eace3ce03676e7d69f6937c70028
Michal Gawik <gawlik.zator@o2.pl>
2024-07-28 09:32:48 +0200

    add query

--- components/ChartsContainer.tsx
+++ components/ChartsContainer.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+// 'use client'
+
+import { getChartsDataAction } from '@/utils/actions'
+import { useQuery } from '@tanstack/react-query'
+import {
+	BarChart,
+	Bar,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	ResponsiveContainer,
+} from 'recharts'
 
 const ChartsContainer = () => {
+	const { data, isPending } = useQuery({
+		queryKey: ['charts'],
+		queryFn: () => getChartsDataAction(),
+	})
+
 	return <div>ChartsContainer</div>
 }
 
